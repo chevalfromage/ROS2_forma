6c823788f443a7cf6b4634be56f3542291b36171
chevalfromage <ant.thiery@free.fr>
2025-10-27 11:45:18 +0100

    sender reciever ok

---
+++ ros2_ws/src/ex1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'ex1'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='student',
+    maintainer_email='ant.thiery@free.fr',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+                'talker = ex1.publisher_member_function:main',
+                'listener = ex1.subscriber_member_function:main',
+        ],
+    },
+)
